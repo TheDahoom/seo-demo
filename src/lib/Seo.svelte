cfaa8747d2eff498c59403b43ddbbf40f32646e2
Dahoom <Rozimo56@gmail.com>
2024-04-06 15:35:37 +0300

    back to normal Seo

--- src/lib/Seo.svelte
+++ src/lib/Seo.svelte
@@ -1,5 +1,4 @@
 <script>
-    import { browser } from "$app/environment";
     import { page } from "$app/stores";
     
     export let title = "";
@@ -10,14 +9,14 @@
     export let twitter = true;
     export let openGraph = true;
     export let schemaOrg = false;
-    export let imageUARL = "";
+    export let imageURL = "";
     export let logo = "";
     export let author = "";
     export let socials = [];
     export let name = "";
 
     let socialsString = socials.map(e => e.url).join(", ");
-    let imageURL = $page.url.origin + imageUARL;
+
     let jsonLd = {
         "@context": "https://schema.org",
         "@type":  ['Person', 'Organization'],
@@ -41,69 +40,57 @@
     ${'<'}/script>`;
 </script>
 <svelte:head>
-    {#if browser}
-        {#if document.querySelector('meta[name="robots"]') === null}
+    {#if title !== ""}
         <meta name="robots" content={index ? "index, follow" : "noindex"} />
-        {/if}
-        {#if title !== "" && document.querySelector('meta[name="title"]') === null}
-            <title>{title}</title>
-        {/if}
-        {#if description !== "" && document.querySelector('meta[name="description"]') === null}
-            <meta name="description" content="{description}" />
-        {/if}
-        {#if keywords !== "" && document.querySelector('meta[name="keywords"]') === null}
-            <meta name="keywords" content="{keywords}" />
-        {/if}
-        {#if author !== "" && document.querySelector('meta[name="author"]') === null}
-            <meta name="author" content="{author}" />
-        {/if}
-        
-        <!-- Open Graph Meta Tags -->
-        {#if openGraph}
-            {#if siteName !== "" && document.querySelector('meta[property="og:site_name"]') === null}
-                <meta property="og:site_name" content="{siteName}">
-            {/if}        
+        <title>{title}</title>
+    {/if}
+    {#if description !== ""}
+        <meta name="description" content="{description}" />
+    {/if}
+    {#if keywords !== ""}
+        <meta name="keywords" content="{keywords}" />
+    {/if}
+    {#if author !== ""}
+        <meta name="author" content="{author}" />
+    {/if}
+    
+    <!-- Open Graph Meta Tags -->
+    {#if openGraph}
+        {#if siteName !== ""}
+            <meta property="og:site_name" content="{siteName}">
+        {/if}        
+        {#if title !== ""}
             <meta property="og:url" content="{$page.url}">
             <meta property="og:type" content="website">
-            {#if title !== "" && document.querySelector('meta[property="og:title"]') === null}
-                <meta property="og:title" content="{title}">
-            {/if}
-            {#if description !== "" && document.querySelector('meta[property="og:description"]') === null}
-                <meta property="og:description" content="{description}">
-            {/if}
-            {#if imageURL !== "" && document.querySelector('meta[property="og:image"]') === null}
-                <meta property="og:image" content="{imageURL}">
-            {/if}
+            <meta property="og:title" content="{title}">
         {/if}
+        {#if description !== ""}
+            <meta property="og:description" content="{description}">
+        {/if}
+        {#if imageURL !== ""}
+            <meta property="og:image" content="{imageURL}">
+        {/if}
+    {/if}
 
-        <!-- Twitter Meta Tags -->
-        {#if twitter}
-            {#if document.querySelector('meta[name="twitter:card"]') === null}
+    <!-- Twitter Meta Tags -->
+    {#if twitter}
+        {#if title !== ""}
             <meta name="twitter:card" content="summary_large_image">
-            {/if}
-            {#if document.querySelector('meta[property="twitter:domain"]') === null}
             <meta property="twitter:domain" content="{$page.url.host}">
-            {/if}
-            {#if document.querySelector('meta[property="twitter:url"]') === null}
             <meta property="twitter:url" content="{$page.url}">
-            {/if}
-            {#if title !== "" && document.querySelector('meta[name="twitter:title"]') === null}
-                <meta name="twitter:title" content="{title}">
-            {/if}
-            {#if description !== "" && document.querySelector('meta[name="twitter:description"]') === null}
-            <meta name="twitter:description" content="{description}">
-            {/if}
-            {#if imageURL !== "" && document.querySelector('meta[name="twitter:image"]') === null}
-            <meta name="twitter:image" content="{imageURL}">
-            {/if}
+            <meta name="twitter:title" content="{title}">
         {/if}
+        {#if description !== ""}
+        <meta name="twitter:description" content="{description}">
+        {/if}
+        {#if imageURL !== ""}
+        <meta name="twitter:image" content="{imageURL}">
+        {/if}
+    {/if}
 
-        <!-- JSON-LD Schema -->
-        {#if schemaOrg || socials != [] || logo !== "" || name !== ""}
-            {#if document.querySelector('script[type="application/ld+json"]') === null}
-                {@html jsonLdScript}
-            {/if}
-        {/if}
+    <!-- JSON-LD Schema -->
+    {#if schemaOrg || socials != [] || logo !== "" || name !== ""}
+        {@html jsonLdScript}
     {/if}
 </svelte:head>
 
