53d46adc2d7ffe3fdf212938a5d1325ae12a4f9a
Dahoom <Rozimo56@gmail.com>
2024-04-06 06:01:46 +0300

    add init for shell codeblock

--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -4,6 +4,6 @@
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
         <h2 class="h2">Getting started</h2>
-        <CodeBlock language="html" code={`<div>This is meta</div>`}></CodeBlock>
+        <CodeBlock language="shell" code={`npm i -D sk-seo`}></CodeBlock>
     </div>
 </div>
