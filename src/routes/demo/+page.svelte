773a8086fe9feda641d0721514b87c0363024267
Dahoom <Rozimo56@gmail.com>
2024-04-06 04:14:37 +0300

    init pages

---
+++ src/routes/demo/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Seo from 'sk-seo';
+</script>
+<Seo 
+    title="Demo"
+    description="demo page for sk-seo package"
+    keywords="sveltekit, seo, skeleton"
+/>
---
+++ src/routes/documentation/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    // Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+</script>
