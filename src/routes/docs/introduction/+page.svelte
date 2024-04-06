0cd74f08baf493de611f6a63f8dfd02ca2ba8a26
Dahoom <Rozimo56@gmail.com>
2024-04-06 06:39:57 +0300

    init intro

---
+++ src/routes/docs/introduction/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Seo from 'sk-seo';
+</script>
+<Seo 
+    title="Introduction"
+    description="Introduction to sk-seo"
+    keywords="sveltekit, seo, skeleton, intro, introduction"
+/>
+
