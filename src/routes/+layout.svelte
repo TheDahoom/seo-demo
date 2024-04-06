0c77cb1bc5a392681f610f1e54a46ec40a0ae5f4
Dahoom <Rozimo56@gmail.com>
2024-04-06 06:09:29 +0300

    align center

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -42,5 +42,9 @@
 		</AppBar>
 	</svelte:fragment>
 	<!-- Page Route Content -->
-	<slot />
+	<div class="container h-full mx-auto flex justify-center items-center">
+		<div class="space-y-10 text-center flex flex-col items-center">
+			<slot />
+			</div>
+	</div>
 </AppShell>
