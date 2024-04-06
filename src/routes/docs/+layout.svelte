155aa3844c68de524fe9e697d37a770b08058895
Dahoom <Rozimo56@gmail.com>
2024-04-06 06:09:18 +0300

    register shell

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -4,10 +4,12 @@
 	import 'highlight.js/styles/github-dark.css';
 	import { storeHighlightJs } from '@skeletonlabs/skeleton';
 	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import shell from 'highlight.js/lib/languages/shell';
 	import javascript from 'highlight.js/lib/languages/javascript';
 	import typescript from 'highlight.js/lib/languages/typescript';
 
 	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('shell', shell);
 	hljs.registerLanguage('javascript', javascript);
 	hljs.registerLanguage('typescript', typescript);
 	storeHighlightJs.set(hljs);
