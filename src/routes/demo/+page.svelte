1f6d5210c11dba3f9497479f087da99904688bd0
Dahoom <Rozimo56@gmail.com>
2024-04-06 13:18:13 +0300

    test demo with image

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,8 +1,10 @@
 <script>
-    import Seo from 'sk-seo';
+    // import Seo from 'sk-seo';
+    import Seo from '$lib/Seo.svelte';
 </script>
 <Seo 
     title="Demo"
     description="demo page for sk-seo package"
     keywords="sveltekit, seo, skeleton"
+    imageURL="https://seo.dahoom.com/seo.png"
 />
