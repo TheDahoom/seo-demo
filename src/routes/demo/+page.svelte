1b1e2ea3156c099ce8c8754591504a6080b547ca
Dahoom <Rozimo56@gmail.com>
2024-04-08 02:26:04 +0300

    init fakecodeblock

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,14 +1,17 @@
 <script>
-    // import Seo from 'sk-seo';
-    import Seo from '$lib/Seo.svelte';
+  import FakeCodeBlock from './FakeCodeBlock.svelte';
+
+    import Seo from 'sk-seo';
+    import { CodeBlock } from '@skeletonlabs/skeleton';
+
+    // import Seo from '$lib/Seo.svelte';
 </script>
 <Seo 
     title="Demo"
     description="demo page for sk-seo package"
     keywords="sveltekit, seo, skeleton"
 />
-<!-- You did it in dahoom -->
 <div class="flex flex-col space-between md:flex-row w-full h-full">
-    <div class="outline-dotted h-full w-full">hi</div>
+    <FakeCodeBlock></FakeCodeBlock>
     <div class="outline-dotted h-full w-full">hi</div>
 </div>
---
+++ src/routes/demo/FakeCodeBlock.svelte
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+
+    <div class="outline-dotted h-full w-full p-4">
+        <div class="codeblock overflow-hidden shadow bg-neutral-900/90  text-sm text-white rounded-container-token" data-testid="codeblock"><header class="codeblock-header text-xs text-white/50 uppercase flex justify-between items-center p-2 pl-4"><span class="codeblock-language">xml</span> <button type="button" class="codeblock-btn btn btn-sm variant-soft !text-white">Copy</button></header> <pre class="codeblock-pre whitespace-pre-wrap break-all p-4 pt-1"><code class="codeblock-code language-xml lineNumbers"><span class="hljs-tag">&lt;<span class="hljs-name">Seo</span> <span class="hljs-attr">title</span>=<span class="hljs-string">"Quick Start"</span> 
+<span class="hljs-attr">description</span>=<input class="hljs-string bg-none" value="{"Quick start webpage for sk-seo"}">
+<span class="hljs-attr">keywords</span>=<span class="hljs-string">"Quickstart, introduction"</span> /&gt;</span></code></pre></div></div>
