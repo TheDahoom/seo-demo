fb10b640737dfe372c74be3abb1ac76b8d61d2bc
Dahoom <Rozimo56@gmail.com>
2024-04-07 23:17:54 +0300

    fixed demo

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,13 +1,14 @@
 <script>
-    import Seo from 'sk-seo';
-    // import Seo from '$lib/Seo.svelte';
+    // import Seo from 'sk-seo';
+    import Seo from '$lib/Seo.svelte';
 </script>
 <Seo 
     title="Demo"
     description="demo page for sk-seo package"
     keywords="sveltekit, seo, skeleton"
 />
-<div class="flex flex-row w-full h-full">
-    <div class="outline-dotted h-full w-full"></div>
-    <div class="outline-dotted h-full w-full"></div>
+<!-- You did it in dahoom -->
+<div class="flex flex-col space-between md:flex-row w-full h-full">
+    <div class="outline-dotted h-full w-full">hi</div>
+    <div class="outline-dotted h-full w-full">hi</div>
 </div>
