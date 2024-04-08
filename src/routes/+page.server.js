04cab7e5788631f22c5eb5a7cd2ef073fd020b63
Dahoom <Rozimo56@gmail.com>
2024-04-08 03:59:03 +0300

    add explanation

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,8 @@
+// This file fetches the package size from an api at build time and returns it as a prop to the page
 export async function load({ fetch }) {
     const res = await fetch('https://bundlephobia.com/api/size?package=sk-seo');
     const data = await res.json();
-    
+
     if (res.ok) {
         return { props: { gzip: data.gzip } };
     } else {
