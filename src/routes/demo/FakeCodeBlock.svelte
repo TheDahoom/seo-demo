21d09416fd4352dbe7a27b633e3688dca66bb9d0
Dahoom <Rozimo56@gmail.com>
2024-04-08 04:08:08 +0300

    style codeblock and input correctly

--- src/routes/demo/FakeCodeBlock.svelte
+++ src/routes/demo/FakeCodeBlock.svelte
@@ -3,6 +3,17 @@
 </script>
 
     <div class="outline-dotted h-full w-full p-4">
-        <div class="codeblock overflow-hidden shadow bg-neutral-900/90  text-sm text-white rounded-container-token" data-testid="codeblock"><header class="codeblock-header text-xs text-white/50 uppercase flex justify-between items-center p-2 pl-4"><span class="codeblock-language">xml</span> <button type="button" class="codeblock-btn btn btn-sm variant-soft !text-white">Copy</button></header> <pre class="codeblock-pre whitespace-pre-wrap break-all p-4 pt-1"><code class="codeblock-code language-xml lineNumbers"><span class="hljs-tag">&lt;<span class="hljs-name">Seo</span> <span class="hljs-attr">title</span>=<span class="hljs-string">"Quick Start"</span> 
-<span class="hljs-attr">description</span>=<input class="hljs-string bg-none" value="{"Quick start webpage for sk-seo"}">
+        <div class="codeblock overflow-hidden shadow bg-neutral-900/90  text-sm text-white rounded-container-token" data-testid="codeblock"><header class="codeblock-header text-xs text-white/50 uppercase flex justify-between items-center p-2 pl-4"><span class="codeblock-language">Svelte</span> <button type="button" class="codeblock-btn btn btn-sm variant-soft !text-white">Copy</button></header> <pre class="codeblock-pre whitespace-pre-wrap break-all p-4 pt-1"><code class="codeblock-code language-xml lineNumbers"><span class="hljs-tag">&lt;<span class="hljs-name">Seo</span> <span class="hljs-attr">title</span>=<span class="hljs-string">"Quick Start"</span> 
+<span class="hljs-attr">description</span>=<input class="hljs-string bg-none" value={"\"Quick start webpage for sk-seo\""}>
 <span class="hljs-attr">keywords</span>=<span class="hljs-string">"Quickstart, introduction"</span> /&gt;</span></code></pre></div></div>
+<style>
+    input.hljs-string {
+        color: inherit;
+        font: inherit;
+        border: none;
+        background: none;
+        padding: 0;
+        margin: 0;
+        outline: none;
+    }
+</style>
