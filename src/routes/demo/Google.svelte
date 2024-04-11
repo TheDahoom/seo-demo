5000ac1ba7a8d2e1fd700bd9e0324dfc2cadaff0
Dahoom <Rozimo56@gmail.com>
2024-04-11 19:25:43 +0300

    attempt 2 with google

--- src/routes/demo/Google.svelte
+++ src/routes/demo/Google.svelte
@@ -1,17 +1,17 @@
 <script>
-    import { writable } from "svelte/store";
+  import { writable } from "svelte/store";
 
-    export let title = writable('Quick Start');
-    export let description = writable('Quick start webpage for sk-seo');
-    export let keywords = writable('Quickstart, introduction');
+  export let title = writable("Quick Start");
+  export let description = writable("Quick start webpage for sk-seo");
+  export let keywords = writable("Quickstart, introduction");
 
-    let dark = false;
+  let dark = false;
 </script>
 
-    <div class="bg-[#202124]">
-        Google
-        
-        {$keywords}
-        <div class="max-w-xl mb-8 font-sans"><div class="group"><a href="/demo" class="text-sml font-OpenSans {dark ? `text-stone-900` : `text-current`}">https://fast.com/</a><a href="/demo"><h2 class="truncate text-xl text-blue-700 group-hover:underline font-OpenSans">{$title}</h2></a></div><p class="line-clamp-2 {dark ? `text-gray-900` : `text-current`} font-OpenSans">{$description}</p></div>
-    </div>
+<div class="bg-[#202124] rounded-xl">
+  <div class="container mx-auto px-4">
 
+</div>
+  {$keywords}
+  create a google search results component
+</div>
