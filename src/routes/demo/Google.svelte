aec69ecaa8a4465c4fabea178d52da380e81ce54
Dahoom <Rozimo56@gmail.com>
2024-04-11 19:36:47 +0300

    remove annoying keywords

--- src/routes/demo/Google.svelte
+++ src/routes/demo/Google.svelte
@@ -12,14 +12,15 @@
   <div class="container mx-auto px-4">
     <div class="py-2">
         <div class="flex overflow-hidden p-0 pb-0 items-center">
-            <div class="bg-white border border-solid block rounded-full h-[28px] w-[28px]">
-                <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"></path></svg>
+            <div class="bg-white border border-solid block rounded-full mr-3 h-[28px] w-[28px]">
+                <svg style="    height: 18px;
+                line-height: 18px;
+                width: 18px;" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"></path></svg>
             </div>
             {$title}
         </div>
     </div>
 </div>
-  {$keywords}
 </div>
 <div class="N54PNb BToiNc cvP2Ce" data-snc="CoXyze">
   <div class="kb0PBd cvP2Ce A9Y9g jGGQ5e" data-snf="x5WNvb" data-snhf="0">
