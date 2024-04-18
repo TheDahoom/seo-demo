6634cfef05e2d52124924065fafcf6e956ebf21f
Dahoom <Rozimo56@gmail.com>
2024-04-18 22:50:13 +0300

    add buttons for follow and star

---
+++ src/lib/buttons/GFollow.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from "svelte";
+    import GithubIcon from "../Icons/GithubIcon.svelte";
+  
+    let followCount;
+
+    onMount(async () => {
+        const response = await fetch('https://api.github.com/repos/TheDahoom/sveltekit-seo');
+        const data = await response.json();
+        followCount = data.stargazers_count;
+        console.log(data);
+    });
+  </script>
+  
+  <a target="_blank" href="https://github.com/TheDahoom" class="input-group w-fit input-group-divider grid-cols-[auto_1fr_auto]">
+    <button class="!px-2">
+        <GithubIcon classes={"scale-[75%] -ml-1"} />
+        Follow
+    </button>
+      <div class="input-group-shim !px-2"> {followCount || 2}</div>
+  </a>
+  
+  
--- src/lib/buttons/GStar.svelte
+++ src/lib/buttons/GStar.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from "svelte";
+  import StarIcon from "../Icons/StarIcon.svelte";
 
 	let starCount;
 
@@ -12,7 +13,10 @@
 </script>
 
 <a target="_blank" href="https://github.com/TheDahoom/sveltekit-seo" class="input-group w-fit input-group-divider grid-cols-[auto_1fr_auto]">
-	<button class=" !px-2">Star</button>
+	<button class="!px-2">
+		<StarIcon classes={"scale-[75%] -ml-1"} />
+		Star
+	</button>
 	<div class="input-group-shim !px-2"> {starCount || 52}</div>
 </a>
 
