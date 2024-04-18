ad725f59e2cad39e45dcd35115ada94414c243f9
Dahoom <Rozimo56@gmail.com>
2024-04-18 19:01:24 +0300

    add Nav to drawer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,7 +30,9 @@
 		'https://github.com/TheDahoom',
 		'https://linkedin.com/in/dahoom'
 	]} />
-<Drawer>contents</Drawer>
+<Drawer>
+	<Nav />
+</Drawer>
 <!-- App Shell -->
 <AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
 	<svelte:fragment slot="header">
