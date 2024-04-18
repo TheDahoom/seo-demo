61cb06cc83f68e9902ccc457d95853d9a6e90e41
Dahoom <Rozimo56@gmail.com>
2024-04-18 18:39:11 +0300

    init nav.svelte

---
+++ src/lib/nav/Nav.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<nav>
+    <ul>
+        <li><a href="/docs">Docs</a></li>
+        <li><a href="/demo">Demo</a></li>
+        <li><a href="/test">Test</a></li>
+    </ul>
+</nav>
