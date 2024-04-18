f4df026577eeba95212618383d97730e6c28f66e
Dahoom <Rozimo56@gmail.com>
2024-04-18 23:02:28 +0300

    no need to fetch github api twice for 1 page load

--- src/lib/buttons/GFollow.svelte
+++ src/lib/buttons/GFollow.svelte
@@ -3,14 +3,6 @@
     import GithubIcon from "../Icons/GithubIcon.svelte";
     
     export let data;
-    let followCount;
-
-    onMount(async () => {
-        const response = await fetch('https://api.github.com/repos/TheDahoom/sveltekit-seo');
-        const data = await response.json();
-        followCount = data.stargazers_count;
-        console.log(data);
-    });
   </script>
   
   <a target="_blank" href="https://github.com/TheDahoom" class="input-group w-fit input-group-divider grid-cols-[auto_1fr_auto]">
@@ -18,7 +10,7 @@
         <GithubIcon classes={"scale-[75%] -ml-1"} />
         Follow
     </button>
-      <div class="input-group-shim !px-2"> {(followCount || data.followCount) || 2}</div>
+      <div class="input-group-shim !px-2"> { data.followCount || 2}</div>
   </a>
   
   
