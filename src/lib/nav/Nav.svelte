97dd7cfcaa91f18644fa9a6f9d5cf06aa0517125
Dahoom <Rozimo56@gmail.com>
2024-04-18 18:42:00 +0300

    add homepage to nav

--- src/lib/nav/Nav.svelte
+++ src/lib/nav/Nav.svelte
@@ -4,6 +4,7 @@
 
 <nav class="list-nav">
     <ul>
+        <li><a href="/">Home</a></li>
         <li><a href="/docs">Docs</a></li>
         <li><a href="/demo">Demo</a></li>
         <li><a href="/test">Test</a></li>
