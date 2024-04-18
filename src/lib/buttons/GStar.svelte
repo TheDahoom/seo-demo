f1d6c40b85e78e9a0eab7118e419843eebb1d2dc
Dahoom <Rozimo56@gmail.com>
2024-04-18 22:25:23 +0300

    progress on Gstar styling

--- src/lib/buttons/GStar.svelte
+++ src/lib/buttons/GStar.svelte
@@ -11,5 +11,8 @@
 	});
 </script>
 
-<button>Star: {starCount || 52}</button>
+<a target="_blank" href="https://github.com/TheDahoom/sveltekit-seo" class="input-group w-fit input-group-divider grid-cols-[auto_1fr_auto]">
+	<button class=" !px-2">Star</button>
+	<div class="input-group-shim !px-2"> {starCount || 52}</div>
+</a>
 
