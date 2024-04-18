5953bc61416b173c8b96e3884994b77d7e8e63c1
Dahoom <Rozimo56@gmail.com>
2024-04-18 20:43:34 +0300

    remove sidebar from home and demo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,8 @@
 	import { AppShell, AppBar, LightSwitch, initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
 	import { page } from '$app/stores';
 
+	$: classesSidebar = $page.url.pathname === '/' || 'demo' ? 'w-0' : 'w-0 lg:w-64';
+
 	//hljs stuff for code blocks
 	import hljs from 'highlight.js/lib/core';
 	import 'highlight.js/styles/github-dark.css';
@@ -51,7 +53,7 @@
 	<Nav />
 </Drawer>
 <!-- App Shell -->
-<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
+<AppShell slotSidebarLeft="bg-surface-500/5 {classesSidebar}">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
 		<AppBar>
@@ -75,10 +77,6 @@
 				<LightSwitch />
 				<a target="_blank" href="https://github.com/Thedahoom/sveltekit-seo" ><GithubIcon /></a>
 				<!-- TODO: set buttons in middle with better styling -->
-				
-				<!-- <a class="btn btn-sm variant-ghost-surface" href="/docs" > docs </a>
-				<a class="btn btn-sm variant-ghost-surface {$page.path === '/' ? 'active' : ''}" href="/demo" > demo </a>
-				<a class="btn btn-sm variant-ghost-surface" href="/test" > test </a> -->
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
