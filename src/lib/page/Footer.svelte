7e8435cb139ab26cce50bf3659f5008e2f1721ec
Dahoom <Rozimo56@gmail.com>
2024-04-19 19:22:47 +0300

    footer from dahoom.com

--- src/lib/page/Footer.svelte
+++ src/lib/page/Footer.svelte
@@ -1,7 +1,19 @@
 <script>
 
 </script>
-<div>
-    logo
-    version:
+<div class="w-full min-h-40 z-50 variant-glass-surface !backdrop-blur-sm relative p-6 md:p-8">
+    <div class="absolute top-0 left-0 pointer-events-none w-full h-full opacity-10 bg-gradient-to-b from-transparent to-surface-500"></div>
+    <div class="flex justify-center md:justify-between pb-2">
+        hello /n dwad
+    </div>
+    <hr class="!border-t-2" />
+    <div class="flex justify-center md:justify-between">
+        <div class=" hidden md:block">hi</div>
+        <div class=" text-center">
+            <a href="/" class="btn">linkedin</a>
+            <a href="/" class="btn">github</a>
+            <a href="/" class="btn">twitter</a>
+            <a href="/" class="btn">youtube</a>
+        </div>
+    </div>
 </div>
