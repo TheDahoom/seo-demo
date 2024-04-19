b1c3919a2c7bec42b0f25a7616303849090eeae8
Dahoom <Rozimo56@gmail.com>
2024-04-19 20:05:50 +0300

    progress on footer

--- src/lib/page/Footer.svelte
+++ src/lib/page/Footer.svelte
@@ -5,17 +5,15 @@
     <div class="absolute top-0 left-0 pointer-events-none w-full h-full opacity-10 bg-gradient-to-b from-transparent to-surface-500"></div>
     <div class="flex justify-center md:justify-between pb-2">
         <div>
-            Sk-seo <span class="badge variant-soft">{version}</span>
+            <strong>Sk-seo</strong> <span class="badge variant-soft">{version}</span>
         </div>
     </div>
     <hr class="!border-t-2" />
     <div class="flex justify-center md:justify-between">
-        <div class=" hidden md:block">hi</div>
-        <div class=" text-center">
-            <a href="/" class="btn">linkedin</a>
-            <a href="/" class="btn">github</a>
-            <a href="/" class="btn">twitter</a>
-            <a href="/" class="btn">youtube</a>
+        <a href="https://github.com/TheDahoom/Sveltekit-seo?tab=MIT-1-ov-file#readme" class="hidden md:block">MIT License</a>
+        <div class="text-center flex flex-row flex-wrap">
+            <a href="https://github.com/TheDahoom/Sveltekit-seo?tab=MIT-1-ov-file#readme" class="btn md:hidden block">MIT License</a>
+            <a href="https://github.com/TheDahoom/Sveltekit-seo" class="btn md:block hidden">github</a>
         </div>
     </div>
 </div>
