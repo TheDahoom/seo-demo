ff1688bb2f6c263dbe118cbbe7d4224b9bcfc978
Dahoom153 <rozimo56@gmail.com>
2024-05-04 10:14:51 +0300

    hardcoded value because bundlephobia is down

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,10 @@
 // This file fetches the package size from an api at build time and returns it as a prop to the page
 export async function load({ fetch }) {
-    const res = await fetch('https://bundlephobia.com/api/size?package=sk-seo');
-    const data = await res.json();
-    const gzip = (data.gzip / 1000).toFixed(1);
-
+    // const res = await fetch('https://bundlephobia.com/api/size?package=sk-seo');
+    // const data = await res.json();
+    // const gzip = (data.gzip / 1000).toFixed(1);
+    const gzip = 2.4;
+    
     const res2 = await fetch('https://api.github.com/repos/TheDahoom/sveltekit-seo');
     const data2 = await res2.json();
     const starCount = data2.stargazers_count;
@@ -13,7 +14,7 @@ export async function load({ fetch }) {
     const followerCount = data3.length;
 
 
-    if (res.ok) {
+    if (res2.ok) {
         return { gzip, starCount, followerCount };
     } else {
         this.error(res.status, data.message);
